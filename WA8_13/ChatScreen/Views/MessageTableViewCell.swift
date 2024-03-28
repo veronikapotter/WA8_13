2fe670ffa11471994ada0064897de2ba6242b8ad
Veronika Potter <potter.v@northeastern.edu>
2024-03-28 13:19:42 -0400

    Fix render messages from DB.

---
+++ WA8_13/ChatScreen/Views/MessageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageTableViewCell.swift
+//  WA8_13
+//
+//  Created by Veronika Potter on 3/28/24.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
